5d6c2946deb033b5165731f26ebf6f871c753db6
Rajarshi Guha <rajarshi.guha@gmail.com>
2010-05-01 10:13:38 -0400

    Added rcdklibs package

---
+++ rcdklibs/R/rcdklibs.R
@@ -0,0 +1,16 @@
+.packageName <- "rcdklibs"
+
+require(rJava, quietly=TRUE)
+
+.First.lib <- function(lib, pkg) {
+    dlp<-Sys.getenv("DYLD_LIBRARY_PATH")
+    if (dlp!="") { # for Mac OS X we need to remove X11 from lib-path
+        Sys.setenv("DYLD_LIBRARY_PATH"=sub("/usr/X11R6/lib","",dlp))
+    }
+
+    jar.cdk <- paste(lib,pkg,"cont","cdk.jar",sep=.Platform$file.sep)
+    jar.jcp <- paste(lib,pkg,"cont","jcp.jar",sep=.Platform$file.sep)
+    .jinit(classpath=c(jar.cdk,jar.jcp))
+}
+    
+
