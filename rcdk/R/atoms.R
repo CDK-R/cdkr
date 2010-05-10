e72c0b0eeaf3d24f189ddb370f494f3dfc387b81
Rajarshi Guha <rajarshi.guha@gmail.com>
2010-05-10 01:22:38 -0400

    Added helper methods to query atom flags

--- rcdk/R/atoms.R
+++ rcdk/R/atoms.R
@@ -2,7 +2,7 @@
 ## atoms <- get.atoms(mol)
 ## coords <- do.call('rbind', lapply(apply, get.point3d))
 
-get.point3d <- function(atom) {
+.valid.atom <- function(atom) {
   if (is.null(attr(atom, 'jclass'))) stop("Must supply an Atom or IAtom object")
   if (!.check.class(atom, "org/openscience/cdk/interfaces/IAtom") &&
       !.check.class(atom, "org/openscience/cdk/Atom"))
@@ -10,7 +10,10 @@ get.point3d <- function(atom) {
 
   if (.check.class(atom, "org/openscience/cdk/Atom"))
     atom <- .jcast(atom, "org/openscience/cdk/interfaces/IAtom")
-  
+  return(atom)
+}
+get.point3d <- function(atom) {
+  atom <- valid.atom(atom)
   p3d <- .jcall(atom, "Ljavax/vecmath/Point3d;", "getPoint3d")
   if (is.jnull(p3d)) return( c(NA,NA,NA) )
   else {
@@ -21,14 +24,7 @@ get.point3d <- function(atom) {
 }
 
 get.point2d <- function(atom) {
-  if (is.null(attr(atom, 'jclass'))) stop("Must supply an Atom or IAtom object")
-  if (!.check.class(atom, "org/openscience/cdk/interfaces/IAtom") &&
-      !.check.class(atom, "org/openscience/cdk/Atom"))
-    stop("Must supply an Atom or IAtom object")
-
-  if (.check.class(atom, "org/openscience/cdk/Atom"))
-    atom <- .jcast(atom, "org/openscience/cdk/interfaces/IAtom")
-
+  atom <- .valid.atom(atom)    
   p3d <- .jcall(atom, "Ljavax/vecmath/Point2d;", "getPoint2d")
   if (is.jnull(p3d)) return( c(NA,NA) )
   else {
@@ -38,25 +34,34 @@ get.point2d <- function(atom) {
 }
 
 get.symbol <- function(atom) {
-  if (is.null(attr(atom, 'jclass'))) stop("Must supply an Atom or IAtom object")
-  if (!.check.class(atom, "org/openscience/cdk/interfaces/IAtom") &&
-      !.check.class(atom, "org/openscience/cdk/Atom"))
-    stop("Must supply an Atom or IAtom object")
-
-  if (.check.class(atom, "org/openscience/cdk/Atom"))
-    atom <- .jcast(atom, "org/openscience/cdk/interfaces/IAtom")
-  
+  atom <- .valid.atom(atom)  
   .jcall(atom, "S", "getSymbol")
 }
 
 get.atomic.number <- function(atom) {
-  if (is.null(attr(atom, 'jclass'))) stop("Must supply an Atom or IAtom object")
-  if (!.check.class(atom, "org/openscience/cdk/interfaces/IAtom") &&
-      !.check.class(atom, "org/openscience/cdk/Atom"))
-    stop("Must supply an Atom or IAtom object")
-
-  if (.check.class(atom, "org/openscience/cdk/Atom"))
-    atom <- .jcast(atom, "org/openscience/cdk/interfaces/IAtom")
-
+  atom <- .valid.atom(atom)
   .jcall(atom, "I", "getAtomicNumber")
+}
+
+get.hydrogen.count <- function(atom) {
+  atom <- .valid.atom(atom)  
+  .jcall(.jcall(atom, "Ljava/lang/Integer;", "getHydrogenCount"), "I", "intValue")
+}
+
+is.aromatic <- function(atom) {
+  atom <- .valid.atom(atom)
+  flag.idx <- .jfield("org/openscience/cdk/CDKConstants", "I", "ISAROMATIC")
+  .jcall(atom, "Z", "getFlag", as.integer(flag.idx))
+}
+
+is.aliphatic <- function(atom) {
+  atom <- .valid.atom(atom)
+  flag.idx <- .jfield("org/openscience/cdk/CDKConstants", "I", "ISALIPHATIC")
+  .jcall(atom, "Z", "getFlag", as.integer(flag.idx))
+}
+
+is.in.ring <- function(atom) {
+  atom <- .valid.atom(atom)
+  flag.idx <- .jfield("org/openscience/cdk/CDKConstants", "I", "ISINRING")
+  .jcall(atom, "Z", "getFlag", as.integer(flag.idx))
 }
