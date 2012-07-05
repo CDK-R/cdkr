450ec9a8d905ae035bdb0e68ddc2519da36920f7
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-07-05 11:35:02 -0400

    Updated matches method to return the matching atom indices

--- rcdk/R/matching.R
+++ rcdk/R/matching.R
@@ -1,4 +1,4 @@
-matches <- function(query, target) {
+matches <- function(query, target, return.matches=FALSE) {
   if (!is.list(target)) target <- list(target)
   if (!all(unlist(lapply(target, class)) == 'jobjRef'))
     stop("targets must be a list of IAtomContainer objects or a single IAtomContainer object")
@@ -6,5 +6,24 @@ matches <- function(query, target) {
   ## make an SQT
   sqt <- new(J("org/openscience/cdk/smiles/smarts/SMARTSQueryTool"), query)
   matchings <- unlist(lapply(target, function(z) sqt$matches(z)))
-  matchings
+
+  matchings <- lapply(target, function(z) {
+    status <- sqt$matches(z)
+    if (status) {
+      mappings <- sqt$getUniqueMatchingAtoms()
+      mappings <- lapply(1:mappings$size(), function(i) {
+        atomIndices <- mappings$get(as.integer(i-1))
+        atomIndinces <- .javalist.to.rlist(atomIndices)
+        sapply(atomIndices, .jsimplify)
+      })
+    } else{
+      mappings <- NULL
+    }
+    return(list(match=status, mapping=mappings))
+  })
+
+  if (!return.matches) return(unlist(lapply(matchings, "[", 1)))
+  else {
+    return(matchings)
+  }
 }
