1b790c14e0b8563acf7c6d9f555e03831849cba8
Rajarshi Guha <rajarshi.guha@gmail.com>
2013-10-05 19:12:01 -0400

    Added a S4 class to represent a feature and its count

--- fingerprint/R/featurefp.R
+++ fingerprint/R/featurefp.R
@@ -1,3 +1,23 @@
+## Define a feature and its count
+setClass("feature",
+         contains = 'integer',
+         representation(feature='character',
+                        count='integer'),
+         validity=function(object) {
+           if (is.na(object@feature) || is.null(object@feature)) return("feature must be a string")
+           if (object@count < 0) return("count must be zero or a positive integer")
+           return(TRUE)
+         },
+         prototype(feature='', count=as.integer(1))
+         )
+setMethod('show', 'feature',
+          function(object) {
+            cat(sprintf('%s:%d', object@feature, object@count), '\n')
+          })
+setMethod('as.character', signature(x='feature'), function(x) sprintf("%s:%d", x@feature, x@count))
+
+
+## A feature fingerprint will be a vector of feature objects
 setClass("featvec",
          representation(features="character",
                         provider="character",
