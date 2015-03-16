43e18bdfce0287cb08e7e25c535ab2f48f958a0d
Rajarshi Guha <rajarshi.guha@gmail.com>
2015-03-15 23:50:43 -0400

    Added roxygen2 docs Added generated Rd files Updated code to explicitly use httr::

--- rbiogrid/R/query.R
+++ rbiogrid/R/query.R
@@ -1,22 +1,57 @@
+#' Retrieve table of organisms and their taxonomy ID's
+#' 
+#' BioGRID supports protein protein interactions in multiple species.
+#' By default queries consider all species, but can be restricted to
+#' consider a specific species. This methods returns all species names
+#' and their corresponding taxonomy ID
+#'
+#' @return A \code{data.frame} with two columns called "taxid" and "organism"
+#' @keywords database
+#' @seealso \code{link{set.access.key}}
+#' @export
 get.organisms <- function() {
   key <- get.access.key()
   if (is.null(key)) stop("Must provide a non-NULL access key")
   url <- sprintf('http://webservice.thebiogrid.org/organisms/?accesskey=%s&format=tab2', key)
-  resp <- GET(url)
+  resp <- httr::GET(url)
   if (resp$status_code != 200) {
     warning(paste("Error retrieving organism list: HTTP Code ", resp$status_code, sep=' ', collapse=''))
     return(NULL)
   }
-  page <- content(resp)
+  page <- httr::content(resp)
   tbl <- read.table(textConnection(page), header=FALSE, as.is=TRUE, sep='\t')
   names(tbl) <- c('taxid', 'organism')
   return(tbl)
 }
 
-
-get.interactions <- function(geneList, species = 9606,
+#' Retrieve interactions for one or more genes.
+#' 
+#' By default this function returns interactions for one or more gene
+#' symbols, separated by the pipe ('|') symbol and considers Homo Sapiens
+#' as the species. See \url{http://wiki.thebiogrid.org/doku.php/biogridrest#list_of_parameters}
+#' for more details.
+#'
+#' @param geneList A character string with pipe delimited series of gene symbols
+#' @param taxId The species to consider. By default Homo Sapiens (9606)
+#' @param searchNames Use identifiers in \code{geneList} and search gene names
+#' @param searchSynonyms Use identifiers in \code{geneList} and search gene synonyms
+#' @param searchIds if \code{TRUE}, identifiers in \code{geneList} are searched for in Entrez Gene ID's.
+#'                  For this to be used \code{searchNames} and \code{searchSynonyms} should be \code{FALSE}
+#' @param searchBiogridIds if \code{TRUE}, identifiers in \code{geneList} are searched for in BioGRID ID's
+#'                  For this to be used \code{searchNames}, \code{searchSynonyms} and \code{searchIds}
+#'                  should be \code{FALSE}
+#' @param selfInteractionsExcluded If \code{TRUE} ignore self interactions
+#' @param includeInteractorInteractions If \code{TRUE}, then interactions between the interactors of the
+#'                  query targets are included in the return value
+#" @param includeEvidence If \code{TRUE} then include reported evidence for the interaction
+#' @param verbose If \code{TRUE} intermediate output is printed such as URLs and interaction counts
+#' @return A \code{data.frame} with interaction information
+#' @seealso \code{link{get.organisms}}, \code{link{set.access.key}}
+#' @keywords database
+#' @export
+get.interactions <- function(geneList, 
                              searchNames = TRUE, searchSynonyms = TRUE, searchIds = FALSE, searchBiogridIds = FALSE,
-                             taxId = 'All',
+                             taxId = 9606,
                              selfInteractionsExcluded = TRUE,
                              includeInteractorInteractions = FALSE, 
                              includeEvidence = TRUE,
@@ -45,21 +80,21 @@ get.interactions <- function(geneList, species = 9606,
 
   ## if it's not in the cache, first query for count of rows
   url <- sprintf('http://webservice.thebiogrid.org/interactions/?accesskey=%s&%s&format=count', key, qs)
-  resp <- GET(url)
+  resp <- httr::GET(url)
   if (resp$status_code != 200) {
     warning(paste("Can't get count for", url, '\n', sep='', collapse=' '))
     return(NULL)
   }
-  count <- as.integer(content(resp))
+  count <- as.integer(httr::content(resp))
   if (verbose) cat("Will retrieve", count, "interactions\n", file=stderr())
 
   url <- sprintf('http://webservice.thebiogrid.org/interactions/?accesskey=%s&%s&format=json', key, qs)
-  resp <- GET(url)
+  resp <- httr::GET(url)
   if (resp$status_code != 200) {
     warning(paste("Error retrieving interactions. HTTP Error", resp$status_code, collapse=' ', sep=''))
     return(NULL)
   } else {
-    page <- content(resp)
+    page <- httr::content(resp)
     page <- do.call(rbind, lapply(page, as.data.frame))
     .set.cache(url, page)
     return(page)
--- rbiogrid/R/setup.R
+++ rbiogrid/R/setup.R
@@ -4,7 +4,26 @@
   assign("cache", list(), envir = .rbiogrid.GlobalEnv)
 }
 
+#' Set your BioGRID access key to be used for subsequent queries
+#' 
+#' To query for interactions from BioGRID requires that each request
+#' include an access key. You should get this when signing up. Set the
+#' access key for this session using this function.
+#'
+#' @param key The users BioGRID access key 
+#'
+#' @keywords authentication
+#' @export
 set.access.key <- function(key) assign("access.key", key, pos = .rbiogrid.GlobalEnv)
+
+#' Get your BioGRID access key 
+#' 
+#' Retrieve the current access key. If it has not been set, the access
+#' key is \code{NULL}
+#'
+#' @return The users BioGRID access key
+#' @keywords authentication
+#' @export
 get.access.key <- function() {
   get("access.key", envir = .rbiogrid.GlobalEnv)
 }
