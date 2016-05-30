b90087342523b33b4be85cdf4ffeaf7678a4b69d
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-05-30 13:16:24 -0400

    the unit test was moved into the .onLoad() function in order to detect java issues when loading the package.

--- rcdk/inst/unitTests/runit.javaversion.R
+++
@@ -1,5 +0,0 @@
-test.Java.version <- function() {
-  jversion <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-  jversionnumeric <- as.numeric(paste0(strsplit(jversion, "\\.")[[1]][1:2], collapse = "."))
-  checkTrue(jversionnumeric >= 1.8)  
-}
