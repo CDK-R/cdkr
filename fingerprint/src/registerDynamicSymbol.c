f4b19c99b8462aa2586e0bb0e64f77583aab54c8
Rajarshi Guha <rajarshi.guha@gmail.com>
2017-09-18 20:39:16 -0400

    cleaned up for R 3.4

---
+++ fingerprint/src/registerDynamicSymbol.c
@@ -0,0 +1,8 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_markovchain(DllInfo* info) {
+  R_registerRoutines(info, NULL, NULL, NULL, NULL);
+  R_useDynamicSymbols(info, TRUE);
+}
